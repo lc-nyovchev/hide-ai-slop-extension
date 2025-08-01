e6fd0bcd405a56fc25a44813a08cc6d26cb33198
nyo <1153009+lc-nyovchev@users.noreply.github.com>
2025-08-01 16:19:56 +0200

    Added gmail as well (for Gemini summaries)

--- src/hide-ai-slop-extension.js
+++ src/hide-ai-slop-extension.js
@@ -1,82 +1,97 @@
 class SlopHiderUtils {
-    async waitForElement(selector) {
-        return new Promise((resolve) => {
-            if (document.querySelector(selector)) {
-                return resolve(document.querySelector(selector))
-            }
-            const observer = new MutationObserver(() => {
-                if (document.querySelector(selector)) {
-                    observer.disconnect()
-                    resolve(document.querySelector(selector))
-                }
-            })
-            observer.observe(document.body, {childList: true, subtree: true})
-        })
-    }
+	async waitForElement(selectorFunc) {
+		return new Promise((resolve) => {
+			if (selectorFunc()) {
+				return resolve(selectorFunc())
+			}
+			const observer = new MutationObserver(() => {
+				if (selectorFunc()) {
+					observer.disconnect()
+					resolve(selectorFunc())
+				}
+			})
+			observer.observe(document.body, { childList: true, subtree: true })
+		})
+	}
 }
 
 class WebsiteSelectorMap {
-    constructor() {
-        this.map = {
-            youtube: '#expandable-metadata',
-            google: 'div[data-mcpr]'
-        }
-    }
+	constructor() {
+		this.map = {
+			youtube: () => {
+				return document.querySelector('#expandable-metadata')
+			},
+			google: () => {
+				return document.querySelector('div[data-mcpr]')
+			},
+			gmail: () => {
+				const geminiLink = document.querySelector('a[href^="https://support.google.com/mail?p=gemini-summary-card"]')
+				if (geminiLink && geminiLink.parentElement && geminiLink.parentElement.parentElement && geminiLink.parentElement.parentElement.parentElement) {
+					return geminiLink.parentElement.parentElement.parentElement
+				}
+			}
+		}
+	}
 }
 
 class SlopHider {
-    constructor(website) {
-        this.websiteSelectorMap = new WebsiteSelectorMap()
-        this.website = website
-        this.slopSelector = this.websiteSelectorMap.map[website]
-        this.slopHiderUtils = new SlopHiderUtils()
-    }
+	constructor(website) {
+		this.websiteSelectorMap = new WebsiteSelectorMap()
+		this.website = website
+		this.slopSelectorFunc = this.websiteSelectorMap.map[website]
+		this.slopHiderUtils = new SlopHiderUtils()
+	}
 
-    removeSlop(slop) {
-        console.log('Deleting slop')
-        slop.remove()
-    }
+	removeSlop(slop) {
+		console.log('Deleting slop')
+		slop.remove()
+	}
 
-    prepareSlopRemovalMessage() {
-        return {
-            type: 'hideAiSlop',
-            removals: 1,
-            website: this.website
-        }
-    }
+	prepareSlopRemovalMessage() {
+		return {
+			type: 'hideAiSlop',
+			removals: 1,
+			website: this.website
+		}
+	}
 
-    hasContent(slop) {
-        return slop && slop.innerHTML && slop.innerHTML.trim() !== ""
-    }
+	hasContent(slop) {
+		return slop && slop.innerHTML && slop.innerHTML.trim() !== ''
+	}
 
-    async startObserving() {
-        const slop = await this.slopHiderUtils.waitForElement(this.slopSelector)
-        if (this.hasContent(slop)) {
-            this.removeSlop(slop)
-            chrome.runtime.sendMessage(this.prepareSlopRemovalMessage())
-        }
-        return new Promise((resolve) => {
-            setTimeout(() => {
-                resolve(this.startObserving())
-            }, 500)
-        });
-    }
+	async startObserving() {
+		const slop = await this.slopHiderUtils.waitForElement(this.slopSelectorFunc)
+		if (this.hasContent(slop)) {
+			this.removeSlop(slop)
+			chrome.runtime.sendMessage(this.prepareSlopRemovalMessage())
+		}
+		return new Promise((resolve) => {
+			setTimeout(() => {
+				resolve(this.startObserving())
+			}, 500)
+		})
+	}
 }
 
 const getSlopHider = () => {
-    const host = window.location.hostname
-    const isYouTube = /(^|\.)youtube\.com$/.test(host) || host === 'm.youtube.com'
-    const isGoogle = /\.google\./.test(host)
-    if (isYouTube) {
-        return new SlopHider('youtube')
-    } else if (isGoogle) {
-        return new SlopHider('google')
-    } else {
-        return null
-    }
+	const host = window.location.hostname
+	const isYouTube = /(^|\.)youtube\.com$/.test(host) || host === 'm.youtube.com'
+	const isGoogle = /\.google\./.test(host)
+	const isGmail = /mail.google.com/.test(host)
+	if (isYouTube) {
+		return new SlopHider('youtube')
+	} else if (isGoogle) {
+		if (isGmail) {
+			return new SlopHider('gmail')
+		} else {
+			return new SlopHider('google')
+		}
+	} else {
+		return null
+	}
 }
 
 const slopHider = getSlopHider()
 if (slopHider) {
-    slopHider.startObserving();
+	slopHider.startObserving()
 }
