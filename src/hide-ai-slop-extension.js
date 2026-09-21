d6797434c92ec2f71f9f5f25d435350bb17edb25
nyo <1153009+lc-nyovchev@users.noreply.github.com>
2026-09-21 09:38:41 +0200

    chore: bumped deps and added MORE selectors for google

--- src/hide-ai-slop-extension.js
+++ src/hide-ai-slop-extension.js
@@ -33,7 +33,7 @@ class WebsiteSelectorMap {
 				if (oldStyle) {
 					return oldStyle
 				}
-				const newStyle = this.getNThParent(document.querySelector('div[data-aquarium]'), 5)
+				const newStyle = this.getNThParent(document.querySelector('div[data-aquarium], div[data-aim], div[data-isaee]'), 5)
 				if (newStyle) {
 					return newStyle
 				}
